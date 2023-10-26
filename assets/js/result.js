98c05aeb53943246d91216ecc8a68f03960341cb
Zeeshan <zeeshan.majeed.it@gmail.com>
2023-10-26 17:22:41 +0500

    update the calculation

--- assets/js/result.js
+++ assets/js/result.js
@@ -1,13 +1,13 @@
 var rating;
 const urlParams = new URLSearchParams(window.location.search);
 const totalMilesRes = parseFloat(urlParams.get("totalMiles")).toFixed(3);
-const totalEmissionRes = parseFloat(urlParams.get("totalEmission")).toFixed(3);
+const totalEmissionRes = (parseFloat(urlParams.get("totalEmission")) * 1000).toFixed(3);
 const totalPerMilesRes = (totalEmissionRes / totalMilesRes).toFixed(3);
 document.getElementById("totalMilesRes").innerHTML = totalMilesRes;
 document.getElementById("totalEmissionRes").innerHTML = totalEmissionRes + "g";
 document.getElementById("totalPerMilesRes").innerHTML = totalPerMilesRes + "g";
 
-if (totalPerMilesRes > 0 && totalPerMilesRes < 80.467) {
+if (totalPerMilesRes >= 0 && totalPerMilesRes < 80.467) {
   rating = "<span class='text-primary'>Low Carbon Footprint</span>";
   document.getElementById("advice").innerHTML =
     "🌱 Eco Warrior: 0 - 80.467 gCO2/mile <br> Way to go! You're treading lightly on Mother Earth!";
